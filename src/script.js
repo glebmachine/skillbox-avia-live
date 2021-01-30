dec9da7d493cfbfd9de1c874c6a87885e1d39c6e
Gleb Mikheev <mikheev@betaagency.ru>
2021-01-30 17:49:07 +0300

    feat(ios): добавил ios приложение

--- src/script.js
+++ src/script.js
@@ -1,7 +1,11 @@
+const { Capacitor } = require('@capacitor/core');
+
 const form = document.getElementById('form')
 const inputTicket = document.getElementById('inputTicket')
 const resetBtn = document.getElementById('resetApp')
 const isElectron = navigator.userAgent.indexOf('Electron') !== -1;
+const isIos = Capacitor.getPlatform() === 'ios';
+const isAndroid = Capacitor.getPlatform() === 'android';
 
 function changeContainer() {
   const containerActive = document.querySelector('.js-container.is-active')
@@ -26,8 +30,15 @@ resetBtn.onclick = changeContainer
 
 if (isElectron) {
   document.body.classList.add('electron');
+  alert('halo marsaine!!!')
 }
 
-if (isElectron) {
-  alert('halo marsaine!!!')
+if (isIos) {
+  document.body.classList.add('ios');
+  // alert('привет айсоеры')
+}
+
+if (isAndroid) {
+  document.body.classList.add('android');
+  alert('привет андройдеры')
 }
