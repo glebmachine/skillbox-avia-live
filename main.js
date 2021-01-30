7b0c3dfa7ca73efb8345228f2fc3dfd835462c9d
Gleb Mikheev <mikheev@betaagency.ru>
2021-01-30 16:12:20 +0300

    feat(electron): добавили лайф-релоад

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const { app, BrowserWindow } = require('electron');
 app.whenReady()
   .then(() => {
     const win = new BrowserWindow({
-      alwaysOnTop: true,
+      // alwaysOnTop: true,
       thickFrame: true,
       frame: false,
       // fullscreen: true,
     });
-    win.loadFile('dist/index.html');
+    // win.loadFile('dist/index.html');
+    win.loadURL('http://localhost:1234');
     win.webContents.openDevTools();
   });
